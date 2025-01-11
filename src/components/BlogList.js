adacd635bc8e54fca3522022644e7cf15c03f577
Icexres <160037346+Icexres@users.noreply.github.com>
2025-01-11 13:08:25 +0545

    Add files via upload

---
+++ src/components/BlogList.js
@@ -0,0 +1,17 @@
+const BlogList = (props) => {
+    const item=props.item;
+    console.log(props,item);
+
+    return (  
+        <div className="blog-list">
+            {item.map((item) => (
+                <div className="item-preview" key={item.id}>
+                    <h2>{item.id}.{item.title}</h2>
+                    <p>written by {item.Author}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+ 
+export default BlogList;
