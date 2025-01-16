2f3042b80855aeafea02baeee74d7ac6c3a5ced5
Icexres <160037346+Icexres@users.noreply.github.com>
2025-01-16 17:24:04 +0545

    Update Home.js
    updates home.js to fetch data from db.json

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,28 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-const [item,setItem]= useState([
-    {title:"The Shining", Stats:"watched", Author:"Stephan King", id: 1},
-    {title:"Shawshank Redemption", Stats:"watched", Author:"Stephan King", id: 2},
-    {title:"The Green Mile", Stats:"watched", Author:"Stephan king", id: 3}
-]);
+const [item,setItem]= useState(null);
 
-    return (  
+
+
+useEffect(()=> {
+    fetch('http://localhost:8000/item')
+        .then(res => {
+            return res.json()
+        })
+        .then(data =>{
+            console.log(data);
+            setItem(data);
+        })
+},[]);
+    
+return (  
         <div className="home">
             <h1>Movie list</h1>
-            <BlogList item={item}></BlogList>
+            {item && <BlogList item={item}></BlogList>}
         </div>
     );
 }
+ 
+export default Home;
