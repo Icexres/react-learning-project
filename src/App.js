cfbade95a764cce7c3cee3169a1819fe0d1c7fa3
Icexres <160037346+Icexres@users.noreply.github.com>
2025-01-18 20:42:16 +0545

    Update App.js
    made a routing system using "npm install react-router-dom@5 " and made changes to route pages from line 9

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
 function App() {
   const title="Movie List";
-  const likes= 50;
   return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+          <div className="container">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+          
+          </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
